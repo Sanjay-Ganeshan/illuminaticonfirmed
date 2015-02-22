883a8b97dc507302f0e54330c5889c4aee75c431
Sanjay-Ganeshan <sangan@live.com>
2015-02-22 12:06:32 -0500

    more crawler

--- pythonCrawler.py
+++ pythonCrawler.py
@@ -3,10 +3,15 @@ import re
 
 str1 = "http://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/Illuminati&namespace=0&limit=50000"
 con1 = httplib.HTTPConnection("en.wikipedia.org")
+con1.connect()
 pathToTool = "/w/index.php?title=Special:WhatLinksHere/"
 keyword = "Illuminati"
 namespaceStr = "&namespace="
 namespace = 0
-limitStr
+limitStr = "&limit="
 limit = 50000
-con1.putrequest("POST",pathToTool + keyword + namespaceStr + str(namespace) + )
+con1.putrequest("GET",pathToTool + keyword + namespaceStr + str(namespace) + limitStr + str(limit))
+
+response = con1.getresponse()
+
+#print response.read()
